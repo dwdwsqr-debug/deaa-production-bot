7cb42ba6f0e1b442bcdae4f21fab673ee66a88f1
dwdwsqr-debug <dwdwsqr@gmail.com>
2026-09-25 18:50:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -226,7 +226,16 @@ def main():
     app.add_handler(MessageHandler(filters.PHOTO, photo))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text))
 
-    app.run_polling()
+    port = int(os.environ.get("PORT", "10000"))
+
+    webhook_url = os.environ["RENDER_EXTERNAL_URL"] + "/telegram"
+
+    app.run_webhook(
+    listen="0.0.0.0",
+    port=port,
+    url_path="telegram",
+    webhook_url=webhook_url,
+)
 
 
 if __name__ == "__main__":
